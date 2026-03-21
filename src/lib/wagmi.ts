ece2ab183b9bdf1008c510b6e48a7812315e788f
Yudiant <yudiantoekosaputroo@gmail.com>
2026-03-21 12:32:20 +0700

    setup sepolia tesnet

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,9 +1,13 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit'
-import { mainnet, polygon, base } from 'wagmi/chains'
+import { sepolia } from 'wagmi/chains'
+import { http } from 'wagmi'
 
 export const config = getDefaultConfig({
   appName: 'TicketPro',
   projectId: 'fe81eb1b17daf3244e518dd16ffe89bc',
-  chains: [mainnet, polygon, base],
+  chains: [sepolia],
+  transports: {
+    [sepolia.id]: http('https://eth-sepolia.g.alchemy.com/v2/7QleYHIyZC-Axa0qwdrRI'),
+  },
   ssr: true,
 })
