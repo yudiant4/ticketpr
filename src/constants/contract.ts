473d6ed647ee2b664af7a74c5b80573379655cb2
Yudiant <yudiantoekosaputroo@gmail.com>
2026-03-22 14:08:05 +0700

    fix: correct contract checksum address

--- src/constants/contract.ts
+++ src/constants/contract.ts
@@ -1,7 +1,7 @@
 import TicketNFTJson from './TicketNFT.json'
 
-export const CONTRACT_ADDRESS = '0x83c8533bBEB920ccd51d3297BBC4d3b5219D0Cb7' as const
+export const CONTRACT_ADDRESS = '0x83c8533bBEB920ccd51d3297BBC4d3b5219D0Cb7'
 
-export const CONTRACT_ABI = TicketNFTJson.abi as const
+export const CONTRACT_ABI = TicketNFTJson.abi
 
 export const SEPOLIA_CHAIN_ID = 11155111
