a43f3605c7a7116c383907f891d573511e995f99
Yudiant <yudiantoekosaputroo@gmail.com>
2026-03-24 14:00:16 +0700

    fix: bypass w3m-button type error and clean ticket page

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,6 +2,17 @@
 
 import Link from 'next/link'
 
+// --- TAMBAHAN: Deklarasi Global agar Vercel mengenali tag w3m-button ---
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'w3m-button': any;
+      'w3m-network-button': any;
+    }
+  }
+}
+// -----------------------------------------------------------------------
+
 export default function Navbar() {
   return (
     <nav style={{
